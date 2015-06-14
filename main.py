8b3dd2951186350c4f2147cc9287ea406e51621b
bednarekj <justina.bednarek@gmail.com>
2015-06-14 14:59:10 +0200

    Utwórz find_term

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ def fib_nums():
     while True:
         yield b
         a, b = b, a + b
+
+
+def find_term(digit_limit):
+    for term_nr, fib in enumerate(fib_nums(), start=1):
+        if digit_limit <= len(str(fib)):
+            return 'F{0}'.format(term_nr)
+
+
